d7c34610811f62c1d3d21052756a852c4bf5ad40
ajmengistu <a_mengistu@outlook.com>
2019-10-10 17:13:09 -0400

    Added postgresql service as a database & add postgres to Django app

--- src/backend/hackernews/hackernews/settings.py
+++ src/backend/hackernews/hackernews/settings.py
@@ -72,14 +72,15 @@ WSGI_APPLICATION = 'hackernews.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
