648c580120ec4488f6697a841e1ea8a8c42a1021
ajmengistu <a_mengistu@outlook.com>
2019-10-13 18:30:46 -0400

    Added pagination & filtering to Post schema

--- src/backend/hackernews/posts/schema.py
+++ src/backend/hackernews/posts/schema.py
@@ -2,21 +2,46 @@ import graphene
 from graphene_django import DjangoObjectType
 from users.schema import UserType
 from datetime import datetime
+from django.db.models import Q
 
 from .models import Post
-
-
 
 class PostType(DjangoObjectType):
     class Meta:
         model = Post
 
 class Query(graphene.ObjectType):
-    posts = graphene.List(PostType)
+    posts = graphene.List(
+        PostType, 
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
 
-    def resolve_posts(self, info, **kwargs):
-        return Post.objects.all()
+    def resolve_posts(self, info, search=None, first=None, skip=None, **kwargs):
+        qs = Post.objects.all()
+        if search:
+            # If you need to execute more complex queries (for example,
+            # queries with OR statements), you can use Q objects.
+            # https://docs.djangoproject.com/en/2.2/topics/db/queries/
+            # Equivalent to: WHERE question LIKE 'Who%' OR question LIKE 'What%'
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            qs = Post.objects.filter(filter)
 
+        # Pagination: Slice method: slice the results using skip & first:
+        # skip, skips the first n items; first, returns the first n items.
+        if skip:
+            qs = qs[skip:]
+        if first:
+            qs = qs[:first]
+
+        return qs
+
+# Create a new Post with specified arguments 
+# (url, description).
 class CreatePost(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
@@ -50,6 +75,7 @@ class CreatePost(graphene.Mutation):
             created_date=post.created_date
         )
 
+# Update the vote count for the specified Post.
 class UpdateVote(graphene.Mutation):
     post = graphene.Field(PostType)
     user = graphene.Field(UserType)
